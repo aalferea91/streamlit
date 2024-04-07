03216c7aa8ab7f04993f110eebc5e3b53cc08c1d
aalferea91 <92360704+aalferea91@users.noreply.github.com>
2024-04-07 12:13:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
+
 st.header('st.button')
 
 if st.button('Say hello'):
-  st.write('Why hello there')
+     st.write('Why hello there')
 else:
-  st.write('Goodbye')
-
-st.write('Hello world')
+     st.write('Goodbye')
