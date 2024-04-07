4d2ac7b08551a166bf9ad64614acb6586e751f72
aalferea91 <92360704+aalferea91@users.noreply.github.com>
2024-04-07 12:28:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
 import streamlit as st
+from datetime import datetime
+
+df_agg=pd.read_csv('Aggregated_Metrics_By_Video.csv'))
 
 st.header('st.button')
 
