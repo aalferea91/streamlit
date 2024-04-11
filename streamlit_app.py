dfe27683cd10161e3b0c25ff6a824e7bbf29054f
aalferea91 <92360704+aalferea91@users.noreply.github.com>
2024-04-11 20:31:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,20 @@ import streamlit as st
 from datetime import datetime
 
 @st.cache
+
+def parse_date(date_string):
+    try:
+        return datetime.strptime(date_string, '%B %d, %Y')
+    except ValueError:
+        return datetime.strptime(date_string, '%b %d, %Y')
+
 def load_data():
     """ Loads in 4 dataframes and does light feature engineering"""
     df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
     df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
                       'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
                       'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
-    df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'])
+    df_agg['Video publish time'] = df_agg['Video publish time'].apply(parse_date)
     df_agg['Average view duration'] = df_agg['Average view duration'].apply(lambda x: datetime.strptime(x,'%H:%M:%S'))
     df_agg['Avg_duration_sec'] = df_agg['Average view duration'].apply(lambda x: x.second + x.minute*60 + x.hour*3600)
     df_agg['Engagement_ratio'] =  (df_agg['Comments added'] + df_agg['Shares'] +df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views
@@ -21,8 +28,12 @@ def load_data():
     df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
     df_comments = pd.read_csv('Aggregated_Metrics_By_Video.csv')
     df_time = pd.read_csv('Video_Performance_Over_Time.csv')
-    df_time['Date'] = pd.to_datetime(df_time['Date'].astype(str),errors='coerce')
+    #df_time['Date'] = pd.to_datetime(df_time['Date'].astype(str),errors='coerce')
     return df_agg, df_agg_sub, df_comments, df_time 
+
+df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
+df_agg['Video publish time'] = df_agg['Video publish time'].apply(parse_date)
+st.dataframe(df_agg['Video publish time'])
 
 #create dataframes from the function 
 #df_agg, df_agg_sub, df_comments, df_time = load_data()
