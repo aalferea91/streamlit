1cec82817dbc1fdc4d53c09497d540a73d52eeeb
aalferea91 <92360704+aalferea91@users.noreply.github.com>
2024-04-11 20:32:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,9 @@ def load_data():
     return df_agg, df_agg_sub, df_comments, df_time 
 
 df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
+df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
+                      'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
+                      'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
 df_agg['Video publish time'] = df_agg['Video publish time'].apply(parse_date)
 st.dataframe(df_agg['Video publish time'])
 
