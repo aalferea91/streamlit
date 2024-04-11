443915f6a199dcc710f7822487d430e0320bcd37
aalferea91 <92360704+aalferea91@users.noreply.github.com>
2024-04-11 19:34:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def load_data():
     df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
                       'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
                       'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
-    df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'].astype(str), errors='coerce')
+    df_agg['Video publish time'] = pd.to_datetime(df_agg['Video publish time'])
     df_agg['Average view duration'] = df_agg['Average view duration'].apply(lambda x: datetime.strptime(x,'%H:%M:%S'))
     df_agg['Avg_duration_sec'] = df_agg['Average view duration'].apply(lambda x: x.second + x.minute*60 + x.hour*3600)
     df_agg['Engagement_ratio'] =  (df_agg['Comments added'] + df_agg['Shares'] +df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views
@@ -24,13 +24,17 @@ def load_data():
     df_time['Date'] = pd.to_datetime(df_time['Date'].astype(str),errors='coerce')
     return df_agg, df_agg_sub, df_comments, df_time 
 
+df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
+df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes', 'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration','Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
+st.text(df_agg['Video publish time'].dtype)
+
 #create dataframes from the function 
-df_agg, df_agg_sub, df_comments, df_time = load_data()
+#df_agg, df_agg_sub, df_comments, df_time = load_data()
 
 #additional data engineering for aggregated data 
-df_agg_diff = df_agg.copy()
-metric_date_12mo = df_agg_diff['Video publish time'].max() - pd.DateOffset(months =12)
-median_agg = df_agg_diff[df_agg_diff['Video publish time'] >= metric_date_12mo].median()
+#df_agg_diff = df_agg.copy()
+#metric_date_12mo = df_agg_diff['Video publish time'].max() - pd.DateOffset(months =12)
+#median_agg = df_agg_diff[df_agg_diff['Video publish time'] >= metric_date_12mo].median()
 
 
 
