2f49550fa35c8c301ab81f5d1e5cb5f43365bc1c
aalferea91 <92360704+aalferea91@users.noreply.github.com>
2024-04-17 19:29:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,6 +77,12 @@ add_sidebar = st.sidebar.selectbox('Aggregate or Individual Video', ('Aggregate 
 #Show individual metrics 
 if add_sidebar == 'Aggregate Metrics':
     st.write("YouTube Aggregated Data")
+    df_agg_metrics = df_agg[['Video publish time','Views','Likes','Subscribers','Shares','Comments added','RPM(USD)','Average % viewed',
+                             'Avg_duration_sec', 'Engagement_ratio','Views / sub gained']]
+    metric_date_6mo = df_agg_metrics['Video publish time'].max() - pd.DateOffset(months =6)
+    metric_date_12mo = df_agg_metrics['Video publish time'].max() - pd.DateOffset(months =12)
+    metric_medians6mo = df_agg_metrics[df_agg_metrics['Video publish time'] >= metric_date_6mo].median()
+    metric_medians12mo = df_agg_metrics[df_agg_metrics['Video publish time'] >= metric_date_12mo].median()
 
 if add_sidebar == 'Individual Video Analysis':
     videos = tuple(df_agg['Video title'])
