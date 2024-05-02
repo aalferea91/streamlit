3ad931ccbc79011291afd79655c42ce80b5ee269
aalferea91 <92360704+aalferea91@users.noreply.github.com>
2024-05-02 19:32:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -258,3 +258,10 @@ if add_sidebar == 'test':
     
     if cola:
          st.write("Here you go 🥤")
+    st.header('st.latex')
+    
+    st.latex(r'''
+         a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+         \sum_{k=0}^{n-1} ar^k =
+         a \left(\frac{1-r^{n}}{1-r}\right)
+         ''')
